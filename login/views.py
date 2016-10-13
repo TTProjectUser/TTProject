b40a9e8e743a88ab8cbe5814584936abcf86ec2c
baqarah <tomek.kimak@gmail.com>
2016-10-13 13:15:37 +0200

    probuje wrzucic AJAXa z Django

--- login/views.py
+++ login/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 # from django.http import HttpResponse
 
 
+@csrf_exempt
 def index(request):
-    return render(request, 'login/home.html')
+    klik = 'eloszka'
+    if request.method == 'POST':
+        # if 'klik' in request.POST:
+
+        klik = 'dziala'
+    return render(request, 'login/home.html', {'klik': klik})
