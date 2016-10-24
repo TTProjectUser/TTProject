889a1884d91da88987e3da2f884cbe5ce69b68b7
Zych <Artur.Zych@cbre.com>
2016-10-24 11:33:52 +0200

    correct activity placement and size on the calendar grid, activities list filered by user and dates

--- scratch/timetracker/models.py
+++ scratch/timetracker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class Activity(models.Model):
@@ -7,21 +8,25 @@ class Activity(models.Model):
     type = models.CharField(max_length=100)
     started = models.DateTimeField()
     ended = models.DateTimeField(blank=True, null=True)
-    date_created = models.DateTimeField(default=timezone.now())
+    date_created = models.DateTimeField(default=timezone.now)
     last_modification = models.DateTimeField(
                         blank=True, null=True)
     all_day = models.BooleanField(default=False)
-    comment = models.CharField(max_length=300)
+    comment = models.CharField(default='', blank=True, max_length=300)
 
+    @property
+    def get_time_difference_in_px(self):
+        difference = self.ended - self.started
+        return ((difference / datetime.timedelta(minutes=1)) * 2) + 6
 
-    def get_time_difference(self):
-        return self.started - self.ended
-
+    @property
+    def get_starting_position(self):
+        return (((self.started - self.started.replace(hour=6, minute=0, second=0, microsecond=0)) /
+                  datetime.timedelta(minutes=1)) * 2) + 6
 
     @property
     def get_day_abbrv(self):
         return self.started.strftime('%a')
-
 
     def __str__(self):
         return self.type
--- scratch/timetracker/views.py
+++ scratch/timetracker/views.py
@@ -10,7 +10,7 @@ def calendar_view(request):
     def get_current_week(date):
         first_day = date - datetime.timedelta(days=date.weekday())
         last_day = first_day + datetime.timedelta(days=4)
-        return first_day.strftime("%d %b"), last_day.strftime("%d %b %Y")
+        return first_day, last_day
 
 
     def get_hours(last_hour):
@@ -19,7 +19,8 @@ def calendar_view(request):
 
     first_day, last_day = get_current_week(timezone.now())
     day_hours = get_hours(20)
-    activites = Activity.objects.filter(user=request.user)
+    activites = Activity.objects.filter(user=request.user, started__gte=first_day, ended__lte=last_day)
     return render(request, 'timetracker/base.html', {'activites': activites, 'weekdays': WEEKDAYS,
-                                                     'first_day': first_day, 'last_day': last_day,
+                                                     'first_day': first_day.strftime("%d %b"),
+                                                     'last_day': last_day.strftime("%d %b %Y"),
                                                      'hours': day_hours})
